82336114e554f5d37ad9b7576cb4d0c1e6e288ac
Tom Ritchford <tom@swirly.com>
2020-07-26 14:24:25 +0200

    Add convert.sh, convert3.sh, convert_a.sh, convert_b.sh, requirements.txt, sd2.py

---
+++ convert3.sh
@@ -0,0 +1,3 @@
+cd /Users/tom/Documents/audio/openLoop/
+
+mkdir -p mp3/2002/02/23 && lame -f target/2002/02/23/2002-02-23-1.wav mp3/2002/02/23/2002-02-23-1.mp3
---
+++ sd2.py
@@ -0,0 +1,43 @@
+import numpy as np
+import os
+import sys
+import wavio
+import wolk
+from pathlib import Path
+
+FRAMERATE = 44100
+ROOT = Path('/Users/tom/Documents/audio/openLoop')
+SOURCE = ROOT / 'source'
+TARGET = ROOT / 'extracted'
+COUNT = 100
+
+
+def main():
+    os.chdir(ROOT)
+    i = 0
+    for f in wolk.wolk('source', relative=True):
+        if f.suffix != '.Sd2f':
+            continue
+
+        i += 1
+        if i > COUNT:
+            break
+
+        target = 'target' / f.with_suffix('.wav')
+        source = 'source' / f
+        target.parent.mkdir(exist_ok=True, parents=True)
+        read(source, str(target))
+        print(target.absolute())
+
+
+def read(infile, outfile):
+    samples = np.fromfile(infile, dtype='>i2')
+    if len(samples) % 2:
+        samples = samples[:-1]
+
+    samples = samples.reshape(len(samples) // 2, 2)
+    wavio.write(outfile, samples, FRAMERATE, sampwidth=2, scale='none')
+
+
+if __name__ == '__main__':
+    main()
